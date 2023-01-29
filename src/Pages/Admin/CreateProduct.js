c038c6b20da4c1be7f2336ec7fdb26f8ef35782d
Rohan Tiwari <ping.techyrohan@gmail.com>
2023-01-29 21:35:37 +0545

    implemented debouncing search

--- src/Pages/Admin/CreateProduct.js
+++ src/Pages/Admin/CreateProduct.js
@@ -9,8 +9,10 @@ import privateInstance from "../../Axios/PrivateInstance";
 export default function CreateProduct() {
   const [images, setImages] = useState("");
   const [suggestion, setSuggestion] = useState([]);
+  const [suggestionText, setSuggestionText] = useState("");
   const dispatch = useDispatch();
-  const { handleChange, handleSubmit, values, setValues } = useFormik({
+
+  const { handleChange, handleSubmit, values, setFieldValue } = useFormik({
     initialValues: {
       productName: "",
       productDescription: "",
@@ -23,6 +25,7 @@ export default function CreateProduct() {
       form.append("productName", value.productName);
       form.append("productDescription", value.productDescription);
       form.append("price", value.price);
+      form.append("category", suggestionText);
       form.append("stocks", value.stocks);
       for (let i = 0; i < images.length; i++) {
         form.append("image", images[i]);
@@ -49,6 +52,11 @@ export default function CreateProduct() {
       clearTimeout(timer);
     };
   }, [values.category]);
+
+  function handleSuggestionClick(slug) {
+    setSuggestionText(slug);
+    setSuggestion([]);
+  }
 
   return (
     <section className="post-product">
@@ -96,14 +104,24 @@ export default function CreateProduct() {
           </div>
           <div className="input category">
             <label htmlFor="">Category</label>
-            <input type="text" id="category" onChange={handleChange} />
+            <input
+              type="text"
+              id="category"
+              value={suggestionText}
+              onChange={(e) => {
+                setFieldValue("category", e.target.value);
+                setSuggestionText(e.target.value);
+              }}
+            />
             <div
               className={`category-suggest ${
                 suggestion.length > 0 ? "active" : ""
               }`}
             >
               {suggestion.map((sugg) => (
-                <li>{sugg.slug}</li>
+                <li onClick={handleSuggestionClick.bind(null, sugg.slug)}>
+                  {sugg.slug}
+                </li>
               ))}
               {suggestion.length === 0 ?? <span>No suggestions</span>}
             </div>
